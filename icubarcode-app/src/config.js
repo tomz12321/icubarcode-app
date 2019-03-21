726ba2853e30ea4c1867ce4454bbc19f3412c98b
tom.yang <tom.yang@pimwa.com>
2019-03-21 19:23:21 +1100

    Install AWS amplify
    Install AWS amplify

---
+++ icubarcode-app/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://qpivos0pq3.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_N0VZ5L4uk",
+      APP_CLIENT_ID: "2sv8dt3lki5js528d1kfbbk7c2",
+      IDENTITY_POOL_ID: "us-east-1:c4fc9751-6d9a-4153-82c5-1df61dadf073"
+    }
+  };
+  
