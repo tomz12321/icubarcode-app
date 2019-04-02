3c54be227400001f653a770fbbf6a443d6c97ca3
tom.yang <tom.yang@pimwa.com>
2019-04-02 22:36:05 +1100

    Create a login page and setup up AWS Cognito
    Create a login page and setup up AWS Cognito

---
+++ test/src/containers/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+//import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
+//import ControlLabel from '@material-ui/core/ControlLabel';
+import TextField from '@material-ui/core/TextField';
+
+import "./Login.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            {/*<ControlLabel>Email</ControlLabel>*/
+            }
+            <TextField>Email</TextField>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            {/*<ControlLabel>Password</ControlLabel>*/
+            }
+            <TextField>Password</TextField>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
